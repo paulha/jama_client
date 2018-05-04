3cb3931ab06b538f5d5ccc4951d7beb79605e015
Paul Hanchett <pfhanchx@intel.com>
2018-05-04 13:37:47 -0700

    Moved setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setup(
     name='jama_client',
     version='0.1',
     packages=['jama_client'],
-    #package_dir={'': 'jama_client'},
+    # package_dir={'': 'jama_client'},
     url='https://github.com/paulha/jama_client.git',
     license='',
     author='Paul Hanchett',
     author_email='paul.hanchett@gmail.com',
-    description='',
+    description='JAMA Client',
     zip_safe=False
 )
